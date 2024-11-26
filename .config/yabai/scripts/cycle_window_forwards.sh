a19ebb305bb10cda1f754fce83cf09a047bef8b8
alexcolemandata <coleman.g.alex@gmail.com>
2024-11-26 11:34:39 +1100

    delete yabai

--- .config/yabai/scripts/cycle_window_forwards.sh
+++
@@ -1,3 +0,0 @@
-yabai -m query --windows --space \
-  | jq -re "map(select(.minimized != 1)) | sort_by(.frame.y, .frame.x, .id) | reverse | nth(index(map(select(.focused == 1))) - 1).id" \
-  | xargs -I{} yabai -m window --focus {}
