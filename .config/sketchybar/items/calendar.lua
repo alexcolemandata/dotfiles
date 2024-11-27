20594c0b3d3772ffdd005ab1edec6e9db39e585f
alexcolemandata <coleman.g.alex@gmail.com>
2024-11-27 12:29:39 +1100

    sketchybar - remove border around calendar

--- .config/sketchybar/items/calendar.lua
+++ .config/sketchybar/items/calendar.lua
@@ -24,24 +24,7 @@ local cal = sbar.add("item", {
 	update_freq = 30,
 	padding_left = 1,
 	padding_right = 1,
-	background = {
-		color = colors.bg2,
-		border_color = colors.black,
-		border_width = 1,
-	},
 })
-
--- Double border for calendar using a single item bracket
-sbar.add("bracket", { cal.name }, {
-	background = {
-		color = colors.transparent,
-		height = 30,
-		border_color = colors.grey,
-	},
-})
-
--- Padding item required because of bracket
-sbar.add("item", { position = "right", width = settings.group_paddings })
 
 cal:subscribe({ "forced", "routine", "system_woke" }, function(env)
 	cal:set({ icon = os.date("%a %Y-%M-%d"), label = os.date("%H:%M") })
