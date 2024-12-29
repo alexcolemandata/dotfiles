a7ad5bd4ec7a9a2d61d0fe9bf28c0c993713614d
alexcolemandata <coleman.g.alex@gmail.com>
2024-12-30 00:29:07 +1100

    nvim: simplify treesitter config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,48 +1,43 @@
 return {
   {
-    "nvim-treesitter/nvim-treesitter", 
+    "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-      require("nvim-treesitter.configs").setup {
-        ensure_installed = { 
-          -- programming
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup {
+        ensure_installed = {
           "bash",
-          "c", 
+          "c",
           "python",
           "sql",
-          "lua", 
-          -- data/misc
+          "lua",
           "html",
           "csv",
           "json",
+          "regex",
           "toml",
-          "markdown", 
-          -- vim
-          "vim", 
-          "vimdoc", 
-          "query", 
+          "markdown",
+          "vim",
+          "vimdoc",
+          "query",
           "markdown_inline",
-          -- git
           "git_config",
           "git_rebase",
           "gitattributes",
           "gitcommit",
           "gitignore",
         },
+        sync_install = false,
+        modules = { },
         auto_install = false,
+        ignore_install = { },
+        indent = { enable = true },
         highlight = {
           enable = true,
-          disable = function(lang, buf)
-            -- disable treesitter for files over 100KB
-            local max_filesize = 100 * 1024 -- 100 KB
-            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-              return true
-            end
-          end
+          additional_vim_regex_highlighting = false,
         },
-        additional_vim_regex_highlighting = false,
       }
-    end,
+    end
   }
 }
