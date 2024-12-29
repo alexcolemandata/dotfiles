4ae128a8a29e9e4b0c298ae2aae0109c235e531c
alexcolemandata <coleman.g.alex@gmail.com>
2024-12-29 18:28:35 +1100

    nvim: update lazy bootstrap

--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -1,18 +1,20 @@
 -- bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({
+		"git", "clone",
 		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
 		"--branch=stable",
+        lazyrepo,
 		lazypath,
 	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("config.globals")
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 require("config.options")
 require("config.keybinds")
 require("config.theme")
