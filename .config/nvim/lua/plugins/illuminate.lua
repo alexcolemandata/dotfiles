a1114c558929fac67e0116cf8190a8fb71b598ce
alexcolemandata <coleman.g.alex@gmail.com>
2024-12-29 22:24:44 +1100

    nvim: delete illuminate

--- .config/nvim/lua/plugins/illuminate.lua
+++
@@ -1,8 +0,0 @@
--- highlights other instances of the word under the cursor
-return {
-    "RRethy/vim-illuminate",
-    lazy = false,
-    config = function()
-        require('illuminate').configure({})
-    end
-}
