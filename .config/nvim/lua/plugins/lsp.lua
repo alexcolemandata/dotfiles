ee1816d3b93cf44a3cfb9fee0d5f9f6daa58eafe
alexcolemandata <coleman.g.alex@gmail.com>
2024-12-31 14:08:56 +1100

    nvim: more lsp config + trouble/telescope tweaks

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -19,6 +19,25 @@ return {
       local config = require("lspconfig")
       config.lua_ls.setup {}
       config.ruff.setup {}
+      config.basedpyright.setup {}
+
+      -- run auto-formatting on save
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if not client then return end
+
+          if client.supports_method("textDocument/formatting", { 0 }) then
+            -- Format the current buffer on save
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              buffer = args.buf,
+              callback = function()
+                vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+              end,
+            })
+          end
+        end,
+      })
     end,
   }
 }
