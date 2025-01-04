1aa96f14a7a4367ee1e6914303d581ad8afa2a1d
alexcolemandata <coleman.g.alex@gmail.com>
2025-01-05 10:45:31 +1100

    nvim: remove noice

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -1,42 +1,42 @@
 return {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    enabled = true,
-    opts = {},
-    dependencies = {
-        "MunifTanjim/nui.nvim",
-        {
-            "rcarriga/nvim-notify",
-            enabled = false,
-            config = function()
-                require("notify").setup({
-                    background_colour = "#000000",
-                    fps = 60,
-                    render = "compact",
-                    stages = "fade",
-                    timeout = 3500,
-                    top_down = false,
-                })
-            end,
-        },
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  enabled = false,
+  opts = {},
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    {
+      "rcarriga/nvim-notify",
+      enabled = false,
+      config = function()
+        require("notify").setup({
+          background_colour = "#000000",
+          fps = 60,
+          render = "compact",
+          stages = "fade",
+          timeout = 3500,
+          top_down = false,
+        })
+      end,
     },
-    config = require("noice").setup({
-      lsp = {
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-        },
-      },
-      -- you can enable a preset for easier configuration
-      presets = {
-        bottom_search = true, -- use a classic bottom cmdline for search
-        command_palette = true, -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false, -- add a border to hover docs and signature help
-      },
-    })
+  },
+  --config = require("noice").setup({
+  --lsp = {
+  ---- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+  --override = {
+  --["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+  --["vim.lsp.util.stylize_markdown"] = true,
+  --["cmp.entry.get_documentation"] = true,   -- requires hrsh7th/nvim-cmp
+  --},
+  --},
+  ---- you can enable a preset for easier configuration
+  --presets = {
+  --bottom_search = true,           -- use a classic bottom cmdline for search
+  --command_palette = true,         -- position the cmdline and popupmenu together
+  --long_message_to_split = true,   -- long messages will be sent to a split
+  --inc_rename = false,             -- enables an input dialog for inc-rename.nvim
+  --lsp_doc_border = false,         -- add a border to hover docs and signature help
+  --
+  --},
+  --})
 }
-
